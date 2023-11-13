135d972e288e4e338f8fbb2866c88cd88c6a94c5
James-the-1st <jamesgarang.162@gmail.com>
2023-11-13 10:45:38 +0300

    loanprocessing_version1

---
+++ main.py
@@ -0,0 +1,21 @@
+# Get user input
+credit_score = int(input("Enter your credit score: "))
+annual_income = float(input("Enter your annual income: "))
+loan_amount = float(input("Enter the loan amount you're requesting: "))
+
+# Define the loan qualification criteria
+min_credit_score = 650
+min_annual_income = 25000
+max_loan_amount = 100002
+
+# Check if the user qualifies for the loan
+if credit_score >= min_credit_score:
+    if annual_income >= min_annual_income:
+        if loan_amount <= max_loan_amount:
+            print("Congratulations! You qualify for the loan.")
+        else:
+            print("Sorry, your loan amount exceeds the maximum allowed.")
+    else:
+        print("Sorry, your annual income is below the minimum required.")
+else:
+    print("Sorry, your credit score is below the minimum required.")
